3d0f1308019b9e38d65642892c267bf71e0376ce
Никита Целков <nikitacelkov@gmail.com>
2020-03-29 17:53:21 +0300

    Добавьте файлы проекта.

---
+++ NeuralNetworks/Layer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetworks
+{
+    public class Layer
+    {
+        public List<Neuron> Neurons { get; }
+
+        public int Count => Neurons?.Count ?? 0;
+
+        public Layer(List<Neuron> neurons, NeuronType type = NeuronType.Normal)
+        {
+            //TODO: проверить нейроны на соответствие типу
+
+            Neurons = neurons;
+        }
+
+        public List<double> GetSignals()
+        {
+            var result = new List<double>();
+            foreach (var neuron in Neurons)
+            {
+                result.Add(neuron.Output);
+            }
+            return result;
+        }
+
+    }
+}
---
+++ NeuralNetworks/Neuron.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetworks
+{
+    public class Neuron
+    {
+
+        public List<double> Weights { get; }
+
+        public NeuronType NeuronType { get; }
+
+        public double Output { get; private set; }
+
+        public Neuron(int inputCount, NeuronType type = NeuronType.Normal)
+        {
+            //TODO: проверка
+            NeuronType = type;
+            Weights = new List<double>();
+
+            for (var i = 0; i < inputCount; i++) 
+            {
+                Weights.Add(1);
+            }
+        }
+
+        public double FeedForward(List<double> inputs)
+        {
+            var sum = 0.0;
+            for (int i = 0; i < inputs.Count; i++)
+            {
+                sum += inputs[i] * Weights[i];
+            }
+
+            Output = Sigmoid(sum);
+            return Output;
+        }
+
+        private double Sigmoid(double x)
+        {
+           return  1.0 / (1.0 + Math.Pow(Math.E, -x));
+        }
+
+        public void SetWeights(params double[] weights)
+        {
+            //TODO: удалить после возможности удаления сети
+            for (int i = 0; i < weights.Length; i++)
+            {
+                weights[i] = weights[i];
+            }
+        }
+
+        public override string ToString()
+        {
+            return Output.ToString();
+        }
+
+    }
+}
